45fd2546665dfe9ff621f28101f4db90174cd758
Alexey <a.kashutin.dev@gmail.com>
2023-02-09 23:10:50 +0600

    add skills section

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,12 @@ flsFunctions.isWebp()
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+//==============================================================================================================================================
+const allPercents = document.querySelectorAll('[data-percent]')
+
+if(allPercents.length > 0) {
+  for (let percent of allPercents) {
+    percent.innerHTML = percent.dataset.percent
+  }
+}
