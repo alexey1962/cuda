ea24a607382f33207c3755b81c984b969ca65db5
Alexey <a.kashutin.dev@gmail.com>
2023-07-26 22:45:24 +0600

    Add: portfolio section; Fix: skills section

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,8 @@ import * as flsFunctions from "./modules/functions.js"
 flsFunctions.isWebp()
 
 import Swiper, { Navigation, Pagination } from 'swiper';
+import mixitup from 'mixitup';
 
 const swiper = new Swiper();
 
-//==============================================================================================================================================
-const allPercents = document.querySelectorAll('[data-percent]')
-
-if(allPercents.length > 0) {
-  for (let percent of allPercents) {
-    percent.innerHTML = percent.dataset.percent
-  }
-}
+const mixer = mixitup('.portfolio-content');
